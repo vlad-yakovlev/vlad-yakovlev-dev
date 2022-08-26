7d722d2c0ef92fc99fa6672dace349fce38b118f
Vladislav Yakovlev <vlad309523@gmail.com>
2022-08-27 01:28:04 +0400

    Fix warnings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,6 @@ export const MyDocument: FC<DocumentProps> = () => {
   return (
     <Html>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <link
           rel="apple-touch-icon"
           sizes="180x180"
