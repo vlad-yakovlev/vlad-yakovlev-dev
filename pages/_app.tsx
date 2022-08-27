484773ca512b39b889d24d29c533782556366e32
Vladislav Yakovlev <vlad309523@gmail.com>
2022-08-27 14:27:38 +0400

    Fix safari layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <Component {...pageProps} />
     </>
