353a66a709889772a1099c8039c985f4c1094f07
Vladislav Yakovlev <vlad309523@gmail.com>
2023-12-16 19:02:17 +0400

    fix: remove extra pwa properties

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document.js'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
         <link
@@ -23,11 +23,6 @@ export default function Document() {
           content="#18181b"
         />
         <meta name="description" content="Vladislav Yakovlev's website" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
         <link rel="mask-icon" href="/icons/mask-icon.svg" color="#16a34a" />
         <link
           rel="apple-touch-icon"
